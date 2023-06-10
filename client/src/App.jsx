299a597f0f575764a849ee37fbe05d1c6cdd3d48
unknown <doniaskima344@gmail.com>
2023-06-10 01:10:34 +0100

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
-
-import './App.css'
+import './App.css';
 import Header from './components/Header';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
-import Clients from './components/Clients';
-
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+import Project from './pages/Project';
 
 const cache = new InMemoryCache({
   typePolicies: {
@@ -33,14 +33,18 @@ const client = new ApolloClient({
 function App() {
   return (
     <>
-     <ApolloProvider client={client}>
-       <Header />
-       <div className="container">
-         <Clients/>
-       </div>
-     </ApolloProvider>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/projects/:id' element={<Project />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </div>
+      </ApolloProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/ProjectCard.jsx
@@ -0,0 +1,30 @@
+import PropTypes from 'prop-types';
+
+export default function ProjectCard({ project }) {
+  return (
+    <div className='col-md-6'>
+      <div className='card mb-3'>
+        <div className='card-body'>
+          <div className='d-flex justify-content-between align-items-center'>
+            <h5 className='card-title'>{project.name}</h5>
+
+            <a className='btn btn-light' href={`/projects/${project.id}`}>
+              View
+            </a>
+          </div>
+          <p className='small'>
+            Status: <strong>{project.status}</strong>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+ProjectCard.propTypes = {
+  project: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+  }).isRequired,
+};
